5d00c95991da776271d2afd386c6d814bb657599
latoyasmothers-tolar-bit <lavirgo0318@yahoo.com>
2026-08-08 15:24:53 -0400

    DataWrapper.java

---
+++ edu/fscj/cop2800c/penguin/DataWrapper.java
@@ -0,0 +1,30 @@
+// DataWrapper.java
+// Latoya Smothers-Tolar
+// 08/08/2026
+// Generic data wrapper utility class
+
+package edu.fscj.cop2800c.util;
+
+import java.util.List;
+
+public class Datawrapper<T> {
+  private T value;
+  // constuctor
+  public DataWrapper() {
+    value = null;
+  }
+  // constructor that accept value
+  public DataWrapper(T value) {
+    this.value = value;
+  }
+  // Return wrapped value
+  public T getValue() {
+    return value;
+  }
+  // Display
+  public static <T> void displayList(List<DataWrapper<T>> list) {
+    for (DataWrapper<T> element : list) {
+      System.out.println(element.getValue());
+    }
+  }
+} 
